4a3b103b12c5e0308b7f89f294a2bc03357a6eb4
wassertim <wassertim@gmail.com>
2022-12-12 22:42:50 +0000

    Deploying to gh-pages from @ wassertim/frequi-fork@98254844d0ab5d41e088f37dddade67553c5a997 🚀

--- assets/createSeriesDataSimply.571bafd3.js
+++
@@ -1,2 +0,0 @@
-import{G as t,K as c,bf as m,aY as o}from"./install.870b0ea5.js";function S(a,e,n){e=t(e)&&{coordDimensions:e}||c({encodeDefine:a.getEncode()},e);var r=a.getSource(),s=m(r,e).dimensions,i=new o(s,a);return i.initData(r,n),i}export{S as c};
-//# sourceMappingURL=createSeriesDataSimply.571bafd3.js.map
