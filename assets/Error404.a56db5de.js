970b57483021bfc79ac1c7f7600212c3493f221e
wassertim <wassertim@gmail.com>
2023-01-21 18:42:43 +0000

    Deploying to gh-pages from @ wassertim/frequi-fork@42b8aea6f78553f7daf20280f4a6543d836fe476 🚀

--- assets/Error404.a56db5de.js
+++
@@ -1,2 +0,0 @@
-import{_ as r,d as a,r as n,o as s,c,a as e,f as o,b as _,w as h}from"./index.cf317eb3.js";const l=a({name:"Error404"}),d=e("h1",{class:"mb-5"},"404 Error.",-1),p=e("p",{class:"h4"},"Ahhhhhhhh! The page you are looking for does not exist.",-1),i=e("span",null,"the main page",-1);function u(f,m,k,x,E,$){const t=n("router-link");return s(),c("div",null,[d,p,e("p",null,[o(" Don't worry, you can head back to "),_(t,{to:"/"},{default:h(()=>[i]),_:1}),o(". ")])])}var g=r(l,[["render",u]]);export{g as default};
-//# sourceMappingURL=Error404.a56db5de.js.map
---
+++ assets/createSeriesDataSimply.0efcd838.js
@@ -0,0 +1,2 @@
+import{G as t,K as c,bf as m,aY as o}from"./install.a09d43e4.js";function S(a,e,n){e=t(e)&&{coordDimensions:e}||c({encodeDefine:a.getEncode()},e);var r=a.getSource(),s=m(r,e).dimensions,i=new o(s,a);return i.initData(r,n),i}export{S as c};
+//# sourceMappingURL=createSeriesDataSimply.0efcd838.js.map
