bc4f71ba91fccbe3a16ac150eb238986d3aec609
wassertim <wassertim@gmail.com>
2023-11-12 13:18:08 +0000

    Deploying to gh-pages from @ wassertim/frequi-fork@98f38a7256c615d321fb70a4a656ccf16ecd4426 🚀

---
+++ assets/LoginView-e71429ee.js
@@ -0,0 +1,2 @@
+import{_,c as t,b as e,w as c,Z as s,o as a,bh as r}from"./index-20bfe6b3.js";const i={},d={class:"container"};function l(p,f){const o=r,n=s;return a(),t("div",d,[e(n,{header:"Freqtrade bot Login"},{default:c(()=>[e(o,{ref:"loginForm"},null,512)]),_:1})])}const m=_(i,[["render",l],["__scopeId","data-v-e45c9504"]]);export{m as default};
+//# sourceMappingURL=LoginView-e71429ee.js.map
