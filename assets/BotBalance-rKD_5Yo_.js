5f9038d48bf4d2a8f9b49c215ff0ea2463377f69
wassertim <wassertim@gmail.com>
2024-02-26 17:47:20 +0000

    Deploying to gh-pages from @ wassertim/frequi-fork@ea3b4ad6438c0a36450e34cb2d2d54dc5541afc2 🚀

---
+++ assets/BotBalance-rKD_5Yo_.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-9AdT5XgX.js";import"./index-5184G5jG.js";import"./installCanvasRenderer--0m70W6g.js";import"./createSeriesDataSimply-L1Jh3Gtf.js";export{o as default};
+//# sourceMappingURL=BotBalance-rKD_5Yo_.js.map
--- assets/Error404View-zWUszCA5.js
+++
@@ -1,2 +0,0 @@
-import{_ as s,c as a,a as e,e as o,b as n,w as r,M as c,o as _}from"./index-c5WNkoLc.js";const h={},l=e("h1",{class:"mb-5"},"404 Error.",-1),i=e("p",{class:"h4"},"Ahhhhhhhh! The page you are looking for does not exist.",-1),d=e("span",null,"the main page",-1);function p(u,f){const t=c("router-link");return _(),a("div",null,[l,i,e("p",null,[o(" Don't worry, you can head back to "),n(t,{to:"/"},{default:r(()=>[d]),_:1}),o(". ")])])}const k=s(h,[["render",p]]);export{k as default};
-//# sourceMappingURL=Error404View-zWUszCA5.js.map
